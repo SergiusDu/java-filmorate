b4acc0287b64eb1aa628537d81d93ea23e53119e
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-06-09 17:57:54 -0400

    feat(exception): add custom exception hierarchy for domain and validation errors
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

---
+++ src/main/java/ru/yandex/practicum/filmorate/domain/exception/DomainException.java
@@ -0,0 +1,57 @@
+package ru.yandex.practicum.filmorate.domain.exception;
+
+/**
+ Base exception class for domain-level exceptions in the application */
+public abstract class DomainException extends RuntimeException {
+    /**
+     Constructs a new domain exception with the specified detail message
+     @param message the detail message
+     */
+    protected DomainException(String message) {
+        super(message);
+    }
+
+    /**
+     Constructs a new domain exception with no detail message
+     */
+    protected DomainException() {
+    }
+
+    /**
+     Constructs a new domain exception with the specified detail message, cause,
+     suppression enabled or disabled, and writable stack trace enabled or
+     disabled
+     @param message the detail message
+     @param cause the cause of the exception
+     @param enableSuppression whether suppression is enabled or disabled
+     @param writableStackTrace whether the stack trace should be writable
+     */
+    protected DomainException(String message,
+                              Throwable cause,
+                              boolean enableSuppression,
+                              boolean writableStackTrace) {
+        super(message,
+              cause,
+              enableSuppression,
+              writableStackTrace);
+    }
+
+    /**
+     Constructs a new domain exception with the specified cause
+     @param cause the cause of the exception
+     */
+    protected DomainException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     Constructs a new domain exception with the specified detail message and
+     cause
+     @param message the detail message
+     @param cause the cause of the exception
+     */
+    protected DomainException(String message, Throwable cause) {
+        super(message,
+              cause);
+    }
+}

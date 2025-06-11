4e79c924d63ebff09e2fbed73927f3a7b3ead95b
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-06-11 19:09:04 -0400

    feat(domain): add User, Film, and Email models with validation logic
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

---
+++ src/main/java/ru/yandex/practicum/filmorate/domain/model/User.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.filmorate.domain.model;
+
+import ru.yandex.practicum.filmorate.domain.exception.InvalidUserDataException;
+import ru.yandex.practicum.filmorate.domain.model.value.Email;
+import ru.yandex.practicum.filmorate.domain.validation.ValidationUtils;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+/**
+ Represents a user entity containing core user information. All fields are validated during construction.
+ @param id Unique identifier for the user
+ @param email Email address of the user (validated format)
+ @param login Login name of the user (non-blank)
+ @param birthday Birthdate of the user */
+public record User(UUID id,
+                   Email email,
+                   String login,
+                   LocalDate birthday) {
+  /**
+   Validates all fields during record construction.
+   @throws InvalidUserDataException if any required field is null or blank
+   */
+  public User {
+    ValidationUtils.notNull(id,
+                            msg -> new InvalidUserDataException("User id must not be null"));
+    ValidationUtils.notNull(email,
+                            msg -> new InvalidUserDataException("User email must not be null"));
+    ValidationUtils.notBlank(login,
+                             msg -> new InvalidUserDataException("User login must not be blank"));
+    ValidationUtils.notNull(birthday,
+                            msg -> new InvalidUserDataException("User birthday must not be null"));
+  }
+}

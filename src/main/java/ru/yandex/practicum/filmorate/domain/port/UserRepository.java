948a2d9222b3e23a62e5e4c844df3d16df9443c8
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-06-12 15:36:57 -0400

    feat(user-repository): add UserRepository interface and in-memory implementation
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

---
+++ src/main/java/ru/yandex/practicum/filmorate/domain/port/UserRepository.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.filmorate.domain.port;
+
+import ru.yandex.practicum.filmorate.domain.exception.DuplicateResourceException;
+import ru.yandex.practicum.filmorate.domain.exception.ResourceNotFoundException;
+import ru.yandex.practicum.filmorate.domain.model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ Repository interface for managing User entity persistence operations. */
+public interface UserRepository {
+  /**
+   Saves a new user.
+   @param user User to save
+   @return Saved user
+   @throws DuplicateResourceException if user with same ID already exists
+   */
+  User save(User user);
+
+  /**
+   Updates an existing user.
+   @param user User to update
+   @return Updated user
+   @throws ResourceNotFoundException if user not found
+   */
+  User update(User user);
+
+  /**
+   Finds user by ID.
+   @param id User ID to find
+   @return Optional containing user if found, empty otherwise
+   */
+  Optional<User> findById(UUID id);
+
+  /**
+   Retrieves all users.
+   @return List of all users
+   */
+  List<User> findAll();
+}

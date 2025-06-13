3797134bdd9333a415c72a49cc9edef58b046c66
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-06-13 19:38:09 -0400

    feat(film): add request DTOs and validation for film creation and update
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

--- src/main/java/ru/yandex/practicum/filmorate/application/services/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/application/services/FilmService.java
@@ -5,21 +5,32 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.application.port.in.FilmUseCase;
 import ru.yandex.practicum.filmorate.domain.model.Film;
 import ru.yandex.practicum.filmorate.domain.port.FilmRepository;
+import ru.yandex.practicum.filmorate.domain.service.FilmValidationService;
+import ru.yandex.practicum.filmorate.infrastructure.web.dto.CreateFilmRequest;
+import ru.yandex.practicum.filmorate.infrastructure.web.dto.UpdateFilmRequest;
+import ru.yandex.practicum.filmorate.infrastructure.web.mapper.FilmMapper;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class FilmService implements FilmUseCase {
-  FilmRepository filmRepository;
+  final FilmRepository filmRepository;
+  private final FilmValidationService filmValidationService;
 
   @Override
-  public Film addFilm(Film film) {
+  public Film addFilm(CreateFilmRequest request) {
+    Film film = FilmMapper.toDomain(request,
+                                    UUID.randomUUID());
+    filmValidationService.validate(film);
     return filmRepository.save(film);
   }
 
   @Override
-  public Film updateFilm(Film film) {
+  public Film updateFilm(UpdateFilmRequest request) {
+    Film film = FilmMapper.toDomain(request);
+    filmValidationService.validate(film);
     return filmRepository.update(film);
   }
 

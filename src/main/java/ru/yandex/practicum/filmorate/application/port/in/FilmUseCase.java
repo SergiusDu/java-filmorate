3dfb4462f7473bcec789097bec242374f80b49ef
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-06-13 19:38:09 -0400

    feat(film): add request DTOs and validation for film creation and update
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

--- src/main/java/ru/yandex/practicum/filmorate/application/port/in/FilmUseCase.java
+++ src/main/java/ru/yandex/practicum/filmorate/application/port/in/FilmUseCase.java
@@ -1,13 +1,15 @@
 package ru.yandex.practicum.filmorate.application.port.in;
 
 import ru.yandex.practicum.filmorate.domain.model.Film;
+import ru.yandex.practicum.filmorate.infrastructure.web.dto.CreateFilmRequest;
+import ru.yandex.practicum.filmorate.infrastructure.web.dto.UpdateFilmRequest;
 
 import java.util.List;
 
 public interface FilmUseCase {
-  Film addFilm(Film film);
+  Film addFilm(CreateFilmRequest film);
 
-  Film updateFilm(Film film);
+  Film updateFilm(UpdateFilmRequest film);
 
   List<Film> getAllFilms();
 }

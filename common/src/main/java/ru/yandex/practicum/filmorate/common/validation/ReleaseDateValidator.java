2dd5218f9803caee5641ce35063f9a64bb7a81f8
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-06-14 10:38:56 -0400

    feat(validation): add ReleaseDateValidator for validating film release dates
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

---
+++ common/src/main/java/ru/yandex/practicum/filmorate/common/validation/ReleaseDateValidator.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.common.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.common.config.AppValidationProperties;
+
+import java.time.LocalDate;
+
+@Component
+public class ReleaseDateValidator implements ConstraintValidator<ValidReleaseDate, LocalDate> {
+  private final LocalDate earliestDate;
+
+  public ReleaseDateValidator(AppValidationProperties validationProperties) {
+    this.earliestDate = validationProperties.film()
+                                            .releaseDate()
+                                            .earliest();
+  }
+
+  @Override
+  public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
+    return value != null && !value.isBefore(earliestDate) && value.isBefore(LocalDate.now());
+  }
+}

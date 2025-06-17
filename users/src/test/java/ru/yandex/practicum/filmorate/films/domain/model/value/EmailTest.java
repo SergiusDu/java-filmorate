252075c4a6b65c10d329f97cada574f1c9df3762
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-06-17 00:12:03 -0400

    test(EmailTest): remove test for email normalization functionality
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

--- users/src/test/java/ru/yandex/practicum/filmorate/films/domain/model/value/EmailTest.java
+++ users/src/test/java/ru/yandex/practicum/filmorate/films/domain/model/value/EmailTest.java
@@ -1,26 +1,13 @@
 package ru.yandex.practicum.filmorate.films.domain.model.value;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import ru.yandex.practicum.filmorate.common.exception.InvalidUserDataException;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class EmailTest {
-
-  @Test
-  void shouldCreateEmailAndConvertToLowerCase_whenEmailIsValid() {
-    String rawEmail = "Test.User+Alias@Example.COM";
-    String expectedEmail = "test.user+alias@example.com";
-
-    Email email = new Email(rawEmail);
-
-    assertThat(email.email()).isEqualTo(expectedEmail);
-  }
-
   @ParameterizedTest
   @NullAndEmptySource
   @ValueSource(strings = {"  ",

27751fd3c4214b9006cbff08d90709dfcb98f8b0
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-07-18 17:50:48 -0400

    refactor(films): adjust duration unit to minutes and sort genres by ID in FilmMapper
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

--- filmorate-app/src/main/java/ru/yandex/practicum/filmorate/infrastructure/web/mapper/FilmMapper.java
+++ filmorate-app/src/main/java/ru/yandex/practicum/filmorate/infrastructure/web/mapper/FilmMapper.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.infrastructure.web.mapper;
 
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.films.domain.model.Film;
+import ru.yandex.practicum.filmorate.films.domain.model.value.Genre;
 import ru.yandex.practicum.filmorate.films.domain.port.CreateFilmCommand;
 import ru.yandex.practicum.filmorate.films.domain.port.UpdateFilmCommand;
 import ru.yandex.practicum.filmorate.infrastructure.web.dto.CreateFilmRequest;
@@ -9,6 +10,10 @@ import ru.yandex.practicum.filmorate.infrastructure.web.dto.FilmResponse;
 import ru.yandex.practicum.filmorate.infrastructure.web.dto.UpdateFilmRequest;
 
 import java.time.Duration;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class FilmMapper {
@@ -36,7 +41,7 @@ public class FilmMapper {
                     command.name(),
                     command.description(),
                     command.releaseDate(),
-                    Duration.ofSeconds(command.duration()),
+                    Duration.ofMinutes(command.duration()),
                     command.genres(),
                     command.mpa());
   }
@@ -46,19 +51,30 @@ public class FilmMapper {
                     command.name(),
                     command.description(),
                     command.releaseDate(),
-                    Duration.ofSeconds(command.duration()),
+                    Duration.ofMinutes(command.duration()),
                     command.genres(),
                     command.mpa());
   }
 
   public FilmResponse toResponse(Film film) {
+    final Set<Genre> genres = film.genres();
+    final Set<Genre> sortedGenres;
+
+    if (genres == null) {
+      sortedGenres = null;
+    } else {
+      sortedGenres = genres.stream()
+                           .sorted(Comparator.comparing(Genre::id))
+                           .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
     return new FilmResponse(film.id(),
                             film.name(),
                             film.description(),
                             film.releaseDate(),
                             film.duration()
                                 .toMinutes(),
-                            film.genres(),
+                            sortedGenres,
                             film.mpa());
   }
 
@@ -67,7 +83,7 @@ public class FilmMapper {
                     request.name(),
                     request.description(),
                     request.releaseDate(),
-                    Duration.ofSeconds(request.duration()),
+                    Duration.ofMinutes(request.duration()),
                     request.genres(),
                     request.mpa());
   }
@@ -77,7 +93,7 @@ public class FilmMapper {
                     request.name(),
                     request.description(),
                     request.releaseDate(),
-                    Duration.ofSeconds(request.duration()),
+                    Duration.ofMinutes(request.duration()),
                     request.genres(),
                     request.mpa());
   }

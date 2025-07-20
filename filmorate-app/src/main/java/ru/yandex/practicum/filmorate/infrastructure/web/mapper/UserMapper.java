20583205098df6a88768fde6189d8de7d8713bd9
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2025-07-19 21:45:40 -0400

    removes unused method from UserFactory (fromCommand)
    Signed-off-by: Sergei Kanchikov <kanchikov.sergey@gmail.com>

--- filmorate-app/src/main/java/ru/yandex/practicum/filmorate/infrastructure/web/mapper/UserMapper.java
+++ filmorate-app/src/main/java/ru/yandex/practicum/filmorate/infrastructure/web/mapper/UserMapper.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.infrastructure.web.mapper;
 
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.films.domain.model.User;
-import ru.yandex.practicum.filmorate.films.domain.model.value.Email;
-import ru.yandex.practicum.filmorate.films.domain.model.value.Login;
 import ru.yandex.practicum.filmorate.films.domain.port.CreateUserCommand;
 import ru.yandex.practicum.filmorate.films.domain.port.UpdateUserCommand;
 import ru.yandex.practicum.filmorate.infrastructure.web.dto.CreateUserRequest;
@@ -27,22 +25,6 @@ public class UserMapper {
                                  request.login(),
                                  request.name() != null ? request.name() : request.login(),
                                  request.birthday());
-  }
-
-  public User fromCommand(Long id, CreateUserCommand command) {
-    return new User(id,
-                    new Email(command.email()),
-                    new Login(command.login()),
-                    command.name(),
-                    command.birthday());
-  }
-
-  public User fromCommand(UpdateUserCommand command) {
-    return new User(command.id(),
-                    new Email(command.email()),
-                    new Login(command.login()),
-                    command.name(),
-                    command.birthday());
   }
 
   public UserResponse toResponse(User user) {

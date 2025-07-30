f00c3b4c8e56da595048296b42a3080ed19ec5f1
a-buianova <a.a.krotenko@gmail.com>
2025-07-30 22:19:36 +0200

    docs(film-repository): restore JavaDoc comments to match project style

--- films/src/main/java/ru/yandex/practicum/filmorate/films/domain/port/FilmRepository.java
+++ films/src/main/java/ru/yandex/practicum/filmorate/films/domain/port/FilmRepository.java
@@ -1,4 +1,3 @@
-// удален избыточный javadoc
 package ru.yandex.practicum.filmorate.films.domain.port;
 
 import ru.yandex.practicum.filmorate.common.exception.DuplicateResourceException;
@@ -11,16 +10,47 @@ import java.util.Set;
 
 public interface FilmRepository {
 
+  /**
+   * Saves a new film.
+   * @param createFilmCommand command containing film data
+   * @return the saved film
+   * @throws DuplicateResourceException if a film with the same ID already exists
+   */
   Film save(CreateFilmCommand createFilmCommand) throws DuplicateResourceException;
 
+  /**
+   * Updates an existing film.
+   * @param updateCommand command containing updated film data
+   * @return the updated film
+   * @throws ResourceNotFoundException if the film does not exist
+   */
   Film update(UpdateFilmCommand updateCommand) throws ResourceNotFoundException;
 
+  /**
+   * Retrieves all films.
+   * @return list of all films
+   */
   List<Film> findAll();
 
+  /**
+   * Finds film by ID.
+   * @param id film ID to find
+   * @return optional containing film if found, empty otherwise
+   */
   Optional<Film> findById(long id);
 
+  /**
+   * Retrieves films by their IDs.
+   * @param ids set of film IDs to retrieve
+   * @return list of films matching the provided IDs
+   */
   List<Film> getByIds(Set<Long> ids);
 
-  // Retrieves films that were liked by both users and sorts them by the total number of likes in descending order.
+  /**
+   * Retrieves films that were liked by both users and sorts them by the total number of likes in descending order.
+   * @param userId ID of the first user
+   * @param friendId ID of the second user
+   * @return list of commonly liked films sorted by popularity
+   */
   List<Film> findCommonFilmsSortedByLikes(long userId, long friendId);
 }

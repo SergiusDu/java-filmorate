c22cc837d731b0f795485901beadaf9b0e6d83c8
a-buianova <a.a.krotenko@gmail.com>
2025-07-31 23:43:43 +0200

    feat: mark Film as soft-deletable

--- films/src/main/java/ru/yandex/practicum/filmorate/films/domain/factory/FilmFactory.java
+++ films/src/main/java/ru/yandex/practicum/filmorate/films/domain/factory/FilmFactory.java
@@ -11,14 +11,15 @@ import java.time.Duration;
 public class FilmFactory {
   public Film create(long id, CreateFilmCommand command) {
     return Film.builder()
-               .id(id)
-               .name(command.name())
-               .description(command.description())
-               .releaseDate(command.releaseDate())
-               .duration(Duration.ofMinutes(command.duration()))
-               .genres(command.genres())
-               .mpa(command.mpa())
-               .build();
+            .id(id)
+            .name(command.name())
+            .description(command.description())
+            .releaseDate(command.releaseDate())
+            .duration(Duration.ofMinutes(command.duration()))
+            .genres(command.genres())
+            .mpa(command.mpa())
+            .isDeleted(false)
+            .build();
   }
 
   public Film update(UpdateFilmCommand command) {
@@ -30,6 +31,7 @@ public class FilmFactory {
                .duration(Duration.ofMinutes(command.duration()))
                .genres(command.genres())
                .mpa(command.mpa())
-               .build();
+            .isDeleted(false)
+            .build();
   }
 }

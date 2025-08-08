e4a05db20b1f189251b7b5d4546400de5917ee88
a-buianova <a.a.krotenko@gmail.com>
2025-08-08 12:26:48 +0200

    refactor(web): remove ResponseEntity from EventController for consistency

--- filmorate-app/src/main/java/ru/yandex/practicum/filmorate/infrastructure/web/controller/EventController.java
+++ filmorate-app/src/main/java/ru/yandex/practicum/filmorate/infrastructure/web/controller/EventController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.infrastructure.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.events.application.port.in.EventUseCase;
 import ru.yandex.practicum.filmorate.infrastructure.web.dto.EventResponse;
@@ -20,7 +19,7 @@ public class EventController {
     private final EventMapper eventResponseMapper;
 
     @GetMapping("/{userId}/feed")
-    public ResponseEntity<List<EventResponse>> getUserFeed(@PathVariable("userId") Long userId) {
-        return ResponseEntity.ok(eventResponseMapper.toResponseList(eventUseCase.getUserFeed(userId)));
+    public List<EventResponse> getUserFeed(@PathVariable("userId") Long userId) {
+        return eventResponseMapper.toResponseList(eventUseCase.getUserFeed(userId));
     }
 }
